fae1a1a452be839d9d116328f40ec080d0f76fc9
Andreas Evensen <58504549+thesombady@users.noreply.github.com>
2024-04-15 10:24:28 +0200

    removed ipynb

--- Gamma_Spectroscopy/GammaSpectroscopy.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-import numpy as np
-import matplotlib.pyplot as plt
-import scipy
-sys.path.append('./lib')
-#import MCA, fittingFunctions, widgetsHelper
-import Spectrum
--- Gamma_Spectroscopy/lib/__init__.py
+++
@@ -1,2 +0,0 @@
-#from .fittingFunctions import fittingFunctions, ExpFunc, GaussFunc, LineFunc, Exp2Func, Gauss, fit_Gaussian, perform_Gaussian_fit
-#from .MCA import Spectrum, load_spectrum, load_calibrated_spectrum, load_MCS_spectrum
